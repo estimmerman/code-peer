c3ac0a9a460565811a4f9278529db18d10e36564
Eric Timmerman <estimmerman@gmail.com>
2015-11-11 15:03:25 -0500

    passport authentication and secrets

---
+++ config/secrets.js
@@ -0,0 +1,12 @@
+module.exports = {
+
+  db: process.env.MONGODB || process.env.MONGOLAB_URI || 'mongodb://localhost:27017/test',
+
+  sessionSecret: process.env.SESSION_SECRET || 'axolotl',
+
+  sendgrid: {
+    user: process.env.SENDGRID_USER || 'hslogin',
+    password: process.env.SENDGRID_PASSWORD || 'hspassword00'
+  }
+
+};
