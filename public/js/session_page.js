b8d7d09701e981cbc0e9b5d959a52eaa4edb5c56
Eric Timmerman <estimmerman@gmail.com>
2015-11-29 18:23:47 -0500

    allow tab char in code textbox

--- public/js/session_page.js
+++ public/js/session_page.js
@@ -5,4 +5,16 @@ $(document).on('ready', function(){
 	var textareaHeight = $(window).height() - headerHeight - footerHeight - bottomPadding;
 	$('#code').css('height', textareaHeight);
 	$('#chat').css('height', textareaHeight);
+
+	// allow tab character in the code textarea
+	$(document).delegate('#code', 'keydown', function(e){
+		var keyCode = e.keyCode || e.which;
+		if (keyCode == 9) {
+			e.preventDefault();
+			var start = $(this).get(0).selectionStart;
+			var end = $(this).get(0).selectionEnd;
+			$(this).val($(this).val().substring(0, start) + '\t' + $(this).val().substring(end));
+			$(this).get(0).selectionStart = $(this).get(0).selectionEnd = start + 1;
+		}
+	});
 });
