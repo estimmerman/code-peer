65318e208a00bd250eb71f46b6dead7bf6f00036
Eric Timmerman <estimmerman@gmail.com>
2015-11-29 18:30:02 -0500

    show if session is active or not

--- controllers/home.js
+++ controllers/home.js
@@ -1,11 +1,24 @@
+var CodeSession = require('../models/CodeSession');
+
 /**
  * GET /
  * Home page.
  */
 exports.index = function(req, res) {
 	if (req.user.role == 0) {
-		res.render('home/student_home', {
-		    title: 'Home'
+		var codeSession = CodeSession.findOne({ user_id: req.user._id }, function(err, codeSession) {
+		    if (err) return next(err);
+		    if (codeSession) {
+		    	res.render('home/student_home', {
+				    title: 'Home',
+				    activeSession: codeSession.active
+				});
+		    } else {
+		    	res.render('home/student_home', {
+				    title: 'Home',
+				    activeSession: false
+				});
+		    }
 		});
 	} else {
 		res.render('home/tutor_home', {
