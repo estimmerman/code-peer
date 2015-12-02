085e17117809cf483011a8a9713615add8efc78e
Eric Timmerman <estimmerman@gmail.com>
2015-12-02 03:26:06 -0500

    finding active session if tutor is in one

--- controllers/home.js
+++ controllers/home.js
@@ -1,4 +1,5 @@
 var CodeSession = require('../models/CodeSession');
+var helpers = require('../helpers/helpers');
 
 /**
  * GET /
@@ -24,38 +25,53 @@ exports.index = function(req, res) {
 		});
 	// tutor accessing home portal
 	} else {
-		var queryFilters = {
-			'activeUsers': 'this.activeUsers',
-			'timeOrder': null
-		};
-		var userFilters = req.user.filterSettings;
-		if (!userFilters.showFull) {
-			queryFilters.activeUsers = 'this.activeUsers.length < 2';
-		}
-		if (userFilters.timeOrder == 'new') {
-			queryFilters.timeOrder = 'desc';
-		} else {
-			queryFilters.timeOrder = 'asc';
-		}
-		CodeSession.find({ active: true, $where: queryFilters.activeUsers })
+		CodeSession.find({ activeUsers: req.user.id })
 		.populate('user', 'firstName lastName school')
-		.sort({lastStartTime: queryFilters.timeOrder})
-		.exec(function (err, codeSessions) {
+		.exec(function (err, ownSession){
 			if (err) return next(err);
+			var activeSession = [];
+			if (ownSession.length > 0) {
+				activeSession.push(ownSession[0]);
+				activeSession[0].minutesStartedAgo = helpers.getMinutesFromSessionStart(activeSession[0]);
+			}
 
-			codeSessions.forEach(function (session) {
-				if (session.lastStartTime) {
-					var diff = Math.abs(new Date() - session.lastStartTime);
-					var minutes = Math.floor((diff/1000)/60);
-					session.minutesStartedAgo = minutes;
-				} else {
-					session.minutesStartedAgo = "an unknown number of";
+			var queryFilters = {
+				'activeUsers': 'this.activeUsers',
+				'timeOrder': null
+			};
+			var userFilters = req.user.filterSettings;
+			if (!userFilters.showFull) {
+				queryFilters.activeUsers = 'this.activeUsers.length < 2';
+			}
+			if (userFilters.timeOrder == 'new') {
+				queryFilters.timeOrder = 'desc';
+			} else {
+				queryFilters.timeOrder = 'asc';
+			}
+			CodeSession.find({ active: true, $where: queryFilters.activeUsers })
+			.populate('user', 'firstName lastName school')
+			.sort({lastStartTime: queryFilters.timeOrder})
+			.exec(function (err, codeSessions) {
+				if (err) return next(err);
+
+				codeSessions.forEach(function (session) {
+					session.minutesStartedAgo = helpers.getMinutesFromSessionStart(session);
+				});
+
+				// filter out from main list
+				if (activeSession.length > 0) {
+					for(var i = 0; i < codeSessions.length; i++) {
+						if (codeSessions[i].id == activeSession[0].id) {
+							codeSessions.splice(i, 1);
+						}
+					}
 				}
-			});
 
-			res.render('home/tutor_home', {
-				title: 'Home',
-				codeSessions: codeSessions
+				res.render('home/tutor_home', {
+					title: 'Home',
+					codeSessions: codeSessions,
+					activeSession: activeSession
+				});
 			});
 		});
 	}
